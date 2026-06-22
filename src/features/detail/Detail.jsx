51c2a3d2bbe5278550eac694b32e98177a4c0b61
Silaenn <deokeldisilaen@gmail.com>
2026-06-22 20:05:48 +0700

    refactor(detail): remove shared photos and files components
    - Remove .photos CSS class and its related styles
    - Delete Shared photos and Shared Files options from Detail component
    - Simplify the layout by removing unnecessary elements and styles

--- src/features/detail/Detail.jsx
+++ src/features/detail/Detail.jsx
@@ -60,34 +60,7 @@ const Detail = () => {
               <img src="./arrowUp.png" alt="" />
             </div>
           </div>
-          <div className="option">
-            <div className="title">
-              <span>Shared photos</span>
-              <img src="./arrowDown.png" alt="" />
-            </div>
-            <div className="photos">
-              <div className="photoItem">
-                <div className="photoDetail">
-                  <img src="./avatar.png" alt="" />
-                  <span>photo_2024_2.png</span>
-                </div>
-                <img src="./download.png" className="icon" alt="" />
-              </div>
-              <div className="photoItem">
-                <div className="photoDetail">
-                  <img src="./avatar.png" alt="" />
-                  <span>photo_2024_2.png</span>
-                </div>
-                <img src="./download.png" className="icon" alt="" />
-              </div>
-            </div>
-          </div>
-          <div className="option">
-            <div className="title">
-              <span>Shared Files</span>
-              <img src="./arrowUp.png" alt="" />
-            </div>
-          </div>
+
           <button onClick={handleBlock}>
             {isCurrentUserBlocked
               ? "You are Blocked!"
